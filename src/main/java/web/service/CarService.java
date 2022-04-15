0f56c385527c16140ace0161b01c4772f5c64ede
FinallRareStaff <Jeymester@gmail.com>
2022-04-15 22:39:45 +0300

    Create model Car and service CarService

---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,25 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarService {
+    List<Car> cars;
+    {
+        cars=new ArrayList<>();
+        cars.add(new Car("BMW",8,3500000));
+        cars.add(new Car("Mersedes",200,7000000));
+        cars.add(new Car("Skoda Octavia",7,2000000));
+        cars.add(new Car("Tesla",2,4500000));
+        cars.add(new Car("Cadillac",5,9000000));
+    }
+    public List<Car> getCars(int value) {
+        List<Car> result = new ArrayList<>();
+        for (int i = 0; i < value; i++) {
+            result.add(cars.get(i));
+        }
+        return result;
+    }
+}
